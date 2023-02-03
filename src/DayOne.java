7c06483171157d2862f704bb061a5ee1ad038352
bhupintiwari <bhupintiwari@gmail.com>
2023-02-02 21:23:32 -0600

    First Day

---
+++ src/DayOne.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class DayOne {
+    public static void main(String[] args) {
+
+        int count = 0; // counter for vowels
+        Scanner sc = new Scanner(System.in);  // Create a Scanner object
+        System.out.println("Please enter a string: "); // prompt user for input
+        String s = sc.nextLine(); // Read user input
+
+        char[] c = s.toCharArray(); // convert string to char array
+
+        for (char ch : c) { // for each loop to iterate through char array
+            // check if char is a vowel
+            if (ch == 'A' || ch == 'a' || ch == 'E' || ch == 'e' || ch == 'I' || ch == 'i' || ch == 'O' || ch == 'o' || ch == 'U' || ch == 'u') {
+                count++; // increment counter
+            }
+
+        }
+        System.out.println("The total amount of vowels are: " + count); // print total amount of vowels
+    }
+}
